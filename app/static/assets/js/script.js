8bc9cfa8e965708e717a945a1b2991dd71f43e4e
muhammadnuralim <nuralim@pacmannai.com>
2023-03-09 08:59:13 +0700

    first commit

---
+++ app/static/assets/js/script.js
@@ -0,0 +1,10 @@
+const div = document.getElementById('h2');
+const addBtn = document.getElementById('done-btn');
+
+// addBtn.addEventListener('click',()=>{
+//     div.classList.add('done');
+// })
+
+// removeBtn.addEventListener('click',()=>{
+//     div.classList.remove('shadow');
+// })
